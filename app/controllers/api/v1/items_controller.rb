6ac3a4994886ce4541750820c898c2c7b97166f4
Max Glassie <msglassie@gmail.com>
2017-04-13 09:56:51 -0600

    tests pass for delete item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,4 +6,13 @@ class Api::V1::ItemsController < ApplicationController
   def show
     render json: Item.find(params[:id]), each_serializer: ItemSerializer
   end
+
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.delete
+      render :nothing => true, :status => 204, :content_type => 'text/html'
+    else
+      render :status => 404
+    end
+  end
 end
--- spec/requests/api/v1/item_delete_request_spec.rb
+++ spec/requests/api/v1/item_delete_request_spec.rb
@@ -7,11 +7,13 @@ describe "delete request deletes an item" do
     item_2 = Item.create(name: "we_the_best_2", description: "Bless Up_2", image_url: "http://cdn.shopify.com/s/files/1/1055/0970/products/blessu-charger-1_large.jpg?v=1469807910")
     item_3 = Item.create(name: "we_the_best_3", description: "Bless Up_3", image_url: "http://cdn.shopify.com/s/files/1/1055/0970/products/blessu-charger-1_large.jpg?v=1469807910")
 
-    delete '/api/v1/items/2'
+    delete '/api/v1/items/3'
 
-    expect(response).to have_http_status(200)
+    expect(response).to have_http_status(204)
 
-    expect(Item.find(2)).to be(false)
-    expect(Item.find(3)).to be(true)
+    items = Item.all
+
+    expect(items.first.name).to eq("we_the_best_1")
+    expect(items.last.name).to eq("we_the_best_2")
   end
 end
