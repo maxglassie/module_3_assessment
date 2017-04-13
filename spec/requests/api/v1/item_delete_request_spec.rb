67c14149823995e6125f0ba99bc153254f346ff9
Max Glassie <msglassie@gmail.com>
2017-04-13 09:43:47 -0600

    TDD delete request

---
+++ spec/requests/api/v1/item_delete_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "delete request deletes an item" do
+  it "returns a 200 status when item is deleted" do
+    #make some data
+    item_1 = Item.create(name: "we_the_best_1", description: "Bless Up_1", image_url: "http://cdn.shopify.com/s/files/1/1055/0970/products/blessu-charger-1_large.jpg?v=1469807910")
+    item_2 = Item.create(name: "we_the_best_2", description: "Bless Up_2", image_url: "http://cdn.shopify.com/s/files/1/1055/0970/products/blessu-charger-1_large.jpg?v=1469807910")
+    item_3 = Item.create(name: "we_the_best_3", description: "Bless Up_3", image_url: "http://cdn.shopify.com/s/files/1/1055/0970/products/blessu-charger-1_large.jpg?v=1469807910")
+
+    delete '/api/v1/items/2'
+
+    expect(response).to have_http_status(200)
+
+    expect(Item.find(2)).to be(false)
+    expect(Item.find(3)).to be(true)
+  end
+end
