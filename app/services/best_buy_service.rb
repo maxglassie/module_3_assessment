41b5b3cbbb9058027cf281ca6cdaf4793a18887d
Max Glassie <msglassie@gmail.com>
2017-04-13 12:03:39 -0600

    final commit

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -7,7 +7,7 @@ class BestBuyService
   end
 
   def stores
-    response = Faraday.get("https://api.bestbuy.com/v1/stores((postalCode=80202))?&show=city,location,name,phone,storeType&format=json&apiKey=#{@api_key}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(postalCode=80202)?apiKey=#{@api_key}&format=json&show=city,location,name,phone,storeType")
     body = JSON.parse(response.body)
   end
 end
